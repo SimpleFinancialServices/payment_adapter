9ea924206519a4c193a1fa2db936b5d01ea2a1b6
Rafael Mendes <rafaom260@yahoo.com.br>
2018-11-01 23:26:59 -0300

    Spring empty project

---
+++ src/main/java/com/rafaelcorporate/payment_adapter/PaymentAdapterApplication.java
@@ -0,0 +1,12 @@
+package com.rafaelcorporate.payment_adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentAdapterApplication.class, args);
+    }
+}
